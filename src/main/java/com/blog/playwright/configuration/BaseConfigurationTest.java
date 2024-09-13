ff1d346d379c970a9203af30467c883fd31b624f
Celobot <celobot@celonis.com>
2024-09-13 18:37:19 +0200

    Blog-playwright create base project structure

---
+++ src/main/java/com/blog/playwright/configuration/BaseConfigurationTest.java
@@ -0,0 +1,15 @@
+package com.blog.playwright.configuration;
+
+import lombok.NoArgsConstructor;
+import org.junit.jupiter.api.BeforeAll;
+
+import static lombok.AccessLevel.PRIVATE;
+
+@NoArgsConstructor(access = PRIVATE)
+public class BaseConfigurationTest {
+
+    @BeforeAll
+    static void beforeAll() {
+
+    }
+}
